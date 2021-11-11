afc6d1fd183a76dba25778a070c0a9e41d79ec8f
AlaBenAicha <ala.ben.aicha23@gmail.com>
2021-11-11 19:09:56 +0100

    preview modal after 2 swipe down

--- Cast.js
+++ Cast.js
@@ -1,10 +1,5 @@
 import React, { useState, useRef, useCallback, useEffect } from 'react';
-import {
-  View,
-  StyleSheet,
-  Dimensions,
-  FlatList,
-} from 'react-native';
+import { View, StyleSheet, Dimensions, FlatList, Modal, Text, Button } from 'react-native';
 import Post from './PostComponent';
 const Cast = () => {
   const [showModal, setShowModal] = useState(false);
@@ -17,6 +12,7 @@ const Cast = () => {
   const [currentPost, setCurrentPost] = useState(0);
   const [isPaused, setPaused] = useState(false);
   const [scrollEnb, setScrollEnb] = useState(true);
+  const [show, setShow] = useState(true)
 
   const [data, setData] = useState([
     {
@@ -41,17 +37,37 @@ const Cast = () => {
     },
   ]);
 
-  const handleStatusChange = (playbackStatus) => {
+  const handleStatusChange = playbackStatus => {
     setLoading(!playbackStatus.isLoaded);
     setStatus(playbackStatus);
   };
 
+  const [visible, setVisible] = useState(true);
   const count = useRef(0);
-
+  const limitScroll = useRef(0);
+  const indexx = useRef(0);
+  const modalVisible = useRef(false);
+  const listRef = useRef();
   const onViewRef = useRef(({ viewableItems, changed }) => {
     if (changed[0].isViewable) {
       setPaused(false);
       setPlaying(changed[0].item.id);
+
+      if (changed[0].index > indexx.current) {
+        limitScroll.current = 0;
+        indexx.current = changed[0].index;
+      } else if (changed[0].index === indexx.current){indexx.current = changed[0].index;}
+      else {
+        
+        if (limitScroll.current === 1) {
+          setPaused(true);
+          setPlaying(0);
+          modalVisible.current = true;
+        } else {
+          limitScroll.current = 1;
+          indexx.current = changed[0].index;
+        }
+      }
     }
     if (changed[0]?.index < changed[1]?.index) {
       count.current = count.current - 1;
@@ -61,10 +77,16 @@ const Cast = () => {
     setCurrentPost(changed[0].item.id);
   });
 
+  const handle = () => {
+    modalVisible.current = false,
+    listRef.current.scrollToIndex({animated:true, index:indexx.current})
+    setShow(!show)
+  };
+
   const viewConfigRef = React.useRef({ viewAreaCoveragePercentThreshold: 50 });
 
   const togglePlayPause = () => {
-    setPaused((prev) => !prev);
+    setPaused(prev => !prev);
   };
 
   useEffect(() => {
@@ -72,9 +94,12 @@ const Cast = () => {
     setPaused(false);
   }, [viewConfigRef]);
 
+  useEffect(() => { // this only gets triggered when the component mounts
+  }, [modalVisible.current]);
+
   let offset = 0;
 
-  const scrollhandler = (e) => {
+  const scrollhandler = e => {
     var currentOffset = e.nativeEvent.contentOffset.y;
     var direction = currentOffset > offset ? 'down' : 'up';
     offset = currentOffset;
@@ -88,6 +113,7 @@ const Cast = () => {
     <View style={styles.container}>
       <FlatList
         data={data}
+        ref={listRef}
         scrollEnabled={scrollEnb}
         onScroll={scrollhandler}
         onViewableItemsChanged={onViewRef.current}
@@ -102,15 +128,38 @@ const Cast = () => {
             isPaused={isPaused}
           />
         )}
-        keyExtractor={(item) => item.id.toString()}
+        keyExtractor={item => item.id.toString()}
         showsVerticalScrollIndicator={false}
         pagingEnabled
         snapToInterval={Dimensions.get('screen').height}
-        snapToAlignment='center'
-        decelerationRate='fast'
+        snapToAlignment="center"
+        decelerationRate="fast"
         disableIntervalMomentum
       />
 
+      {modalVisible.current === true && (
+        <View style={styles.centeredView}>
+          <Modal
+            animationType="slide"
+            transparent={true}
+            visible={modalVisible.current}
+            onRequestClose={() => {
+              Alert.alert('Modal has been closed.');
+              modalVisible.current === false;
+            }}
+          >
+            <View style={styles.centeredView}>
+              <View style={styles.modalView}>
+                <Text style={styles.modalText}>You need premiun access</Text>
+                <View style={{ flexDirection: 'row' }}>
+                  <Button style={[styles.button, styles.buttonClose]} title="Press me" />
+                  <Button style={[styles.button, styles.buttonOpen]} onPress={() => handle()} title="Press" />
+                </View>
+              </View>
+            </View>
+          </Modal>
+        </View>
+      )}
     </View>
   );
 };
@@ -119,6 +168,51 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 10,
+    height: '40%',
+    width: '80%',
+    alignSelf: 'center',
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonOpen: {
+    backgroundColor: 'red',
+  },
+  buttonClose: {
+    backgroundColor: '#2196F3',
+  },
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
   },
 });
 export default Cast;
